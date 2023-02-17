dfc064e2e1f93528bc29fb2ba9b7dd0bf66e3981
khmalz <akmalrma1710@gmail.com>
2023-02-17 17:09:12 +0700

    Make migration for k-drama

---
+++ app/Http/Controllers/KDramaController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KDrama;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class KDramaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): Response
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KDrama $kDrama): Response
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, KDrama $kDrama): Response
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KDrama $kDrama): Response
+    {
+        //
+    }
+}
